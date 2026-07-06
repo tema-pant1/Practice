b2642b668b6eed41877537c1daafcdbdd4f6d95e
tema-pant1 <tid.midii@mail.ru>
2026-07-06 22:24:14 +0300

    Experiments with variable type

--- any.cpp
+++ any.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
-#include <string>
+
 
 int main()
 {
     using namespace std;
-    string s("Hello");
-    cout << "this is my string - " << s;
-    cout << "\nit is lenght s - " << s.length();
-    cout << "\ns is empty? - " << s.empty();
-    s.clear();
-    cout << "\ns is clear -" << s;
-    s.push_back('k');
-    cout << "\nnow s is - " << s;
-    s.pop_back();
-    s += "world";
-    cout << "\nit is second character of s - " << s[2] << " and this is s -" << s;
-    string b = s;
-    cout << "\nI created b. b equal s - " << b;
-    
+    return 0;
 }
+
