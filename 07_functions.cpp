e5f61cde49f61b7d133ba055a9ff7417ca48b310
tema-pant1 <tid.midii@mail.ru>
2026-07-13 21:31:30 +0300

    Complete exercise 2

--- 07_functions.cpp
+++ 07_functions.cpp
@@ -28,8 +28,48 @@ void calculate_harmonic()
         cout << elem[0] << ", " << elem[1] << " - " << elem[2] << endl;
 }   
 
+vector<int> golf_entering()
+{
+    vector<int> results;
+    int game(0);
+    cout << "Enter results of your golf games (max 10).\n";
+    cout << "To stop entering enter -1\n";
+    cin >> game;
+    while (results.size() < 10 && !cin.fail() && game >= 0)
+    {
+        results.push_back(game);
+        if (results.size() < 10) cin >> game;
+    }
+    return results;
+}
+
+double golf_average(const vector<int> results)
+{
+    double average;
+    for (int elem: results) average+=elem;
+    average /= results.size();
+    return average;
+}
+
+void golf_output(vector<int> results, double average)
+{
+    cout << "Your results:\n";
+    for (int i = 0; (i+1) < results.size(); i++)
+        cout << results[i] << ", ";
+    cout << results[results.size()-1] << endl;
+    cout << "Average result: " << average << endl;
+}
+
+void golf()
+{
+    vector<int> results = golf_entering();
+    double average = golf_average(results);
+    golf_output(results, average);
+
+}
+
 int main()
 {
-    calculate_harmonic();
+    golf();
     return 0;
 }
